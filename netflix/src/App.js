714db0b2a0a4d74c21347d36e2ca01990b3d91ce
Tiago Abranges <tiagoabranges@gmail.com>
2023-01-03 16:24:06 -0300

    feat: created function get movies

---
+++ netflix/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      {/* navbar */}
+      {/* destaques */}
+      {/* em alta */}
+      {/* filmes de cada categoria */}
+    </div>
+  );
+}
+
+export default App;
