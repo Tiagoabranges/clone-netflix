1460ffc7675f6beefdccb5469a7aa17572b196a4
Tiago Abranges <tiagoabranges@gmail.com>
2023-01-03 16:47:16 -0300

    feat: implement app

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,10 +1,19 @@
+import Row from './components/Row';
+import categories from './api';
+
 function App() {
   return (
     <div className="App">
       {/* navbar */}
       {/* destaques */}
       {/* em alta */}
-      {/* filmes de cada categoria */}
+      {categories.map((category) => (
+        <Row
+          key={ category.name }
+          title={ category.title }
+          path={ category.path }
+        />
+      ))}
     </div>
   );
 }
